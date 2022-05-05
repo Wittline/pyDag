e64f40e9108fe5891bfb1b01f9ae843d9a53a386
Wittline <8701464+Wittline@users.noreply.github.com>
2022-05-05 14:58:42 -0500

    Updating task, script_handler, and engine

---
+++ pyDag/engine.py
@@ -0,0 +1,18 @@
+from scripts_handler import ScriptHanlder as sh
+from pyDag.scripts import pyscripts
+
+class Engine:
+
+    def __init__(self, id, params, script):
+        self.id = id
+        self.params = params
+        self.script = script
+    
+    def __get_url_engine():
+
+
+    def run(self, script):
+        sh.format_script
+        self.run()
+
+
--- pyDag/task.py
+++ pyDag/task.py
@@ -1,24 +1,16 @@
 from logging import exception
 from task_state import TaskState
-import time
-import random
+from engine import Engine
 
-class Task:
+
+class Task(Engine):
 
     def __init__(self, id, params, script):
         self.id = id
         self.params = params
         self.script = script
         self.state = TaskState.none
-        
-    def execute_script(self):
-        _script = self.script.split('.')
-        time.sleep(random.randint(2, 5))
-
-        # print("Task id: '{0}' executing script '{1}' with params: '{2}'".format(
-        #     self.id, _script[-1], self.params
-        #     ))
-
+        super().__init__(self, id, params, script)        
         
         
 
